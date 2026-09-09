29ccbfc3a51b8d22da88703f9a5342189477f9bb
RichardFFrank <71610664+RichardFFrank@users.noreply.github.com>
2026-09-09 18:59:57 -0400

    Relied too much on the exception logic.

--- webServer.py
+++ webServer.py
@@ -1,5 +1,6 @@
 # import socket module
 from socket import *
+import os
 # In order to terminate the program
 import sys
 
@@ -35,32 +36,38 @@ def webServer(port=13331):
     try:
       message = connectionSocket.recv(1024)#Fill in start -a client is sending you a message   #Fill in end 
       filename = message.split()[1]
+
+      file = filename[1:]
       
       #opens the client requested file. 
-      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      with open(filename[1:]) as f:
-        # fill in start 
-        # Do SOmething
-        data = f.read()
-        # fill in end
-        
+      if os.path.isfile(file):
+        with open(file) as f:
+          # fill in start 
+          # Do SOmething
+          data = f.read()
+          # fill in end
+          
 
-      #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
-      #Fill in start 
-              
-      #Content-Type is an example on how to send a header as bytes. There are more!
-      response = build_response("HTTP/1.1 200 OK", data)
-      #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+        #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
+        #Fill in start 
+                
+        #Content-Type is an example on how to send a header as bytes. There are more!
+        response = build_response("HTTP/1.1 200 OK", data)
+        #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
 
-      #Fill in end
-               
-      # for i in f: #for line in file
-      #Fill in start - append your html file contents #Fill in end 
-        
-      #Send the content of the requested file to the client (don't forget the headers you created)!
-      #Send everything as one send command, do not send one line/item at a time!
+        #Fill in end
+                
+        # for i in f: #for line in file
+        #Fill in start - append your html file contents #Fill in end 
+          
+        #Send the content of the requested file to the client (don't forget the headers you created)!
+        #Send everything as one send command, do not send one line/item at a time!
 
-      # Fill in start
+        # Fill in start
+
+      else:
+        body = "<html><body><h1>404 Not Found</h1></body></html>"
+        response = build_response("HTTP/1.1 404 Not Found", body)
 
       connectionSocket.sendall(response)
       # Fill in end
